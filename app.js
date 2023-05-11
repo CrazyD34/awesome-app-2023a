246554509bb3e8e0dcecaef628f7477e4c387d54
CrazyD34 <pericopter67@gmail.com>
2023-05-11 14:56:10 +0000

    🖥️Middleware

--- app.js
+++ app.js
@@ -3,8 +3,34 @@ import http from 'http';
 //Importando express
 import express from 'express';
 
+
 //Crear una instancia de express
-const app = express(); //Express es un midlewear, incluyen (request,response)
+const app = express(); //Express es un middleware, incluyen (request,response)
+
+//Registrando el primer middleware con una funcion
+
+app.use((request,response,next) => {
+    console.log("Este es el primer middleware");
+    next();
+});
+
+app.use( (request,response,next) => {
+    console.log("Registrando peticion entrante");
+    console.log(`${request.method} - ${request.url}`)
+    next();
+})
+
+//Respondiendo al cliente
+app.use( (request,response) => {
+    response.send(`
+    <html>
+    <body>
+    <h1>Welcome to Express</h1>
+    <p>This is my awesome app ⭐</p>
+    </body>
+    </html>
+    `);
+})
 
 //Creando el servidor
 const server = http.createServer(app);
