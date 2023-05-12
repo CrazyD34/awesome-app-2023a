c12813134724e84127a802c32e116d0cd66ead2b
CrazyD34 <pericopter67@gmail.com>
2023-05-12 15:44:05 +0000

    Optimización Codigo

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-//Importando el modulo http
-import http from 'http';
 //Importando express
 import express from 'express';
-
 
 //Crear una instancia de express
 const app = express(); //Express es un middleware, incluyen (request,response)
@@ -32,15 +29,12 @@ app.use( (request,response) => {
     `);
 })
 
-//Creando el servidor
-const server = http.createServer(app);
-
 //Definir puertos
 const port = 3000;
 const ip = "0.0.0.0";
 
-//Arrancado el servidor
-server.listen(port,ip, (err) => {
+//Creando el servidor utilizando express y ejecutandolo
+app.listen(port,ip, (err) => {
     console.log("👂 Sirviendo en http://localhost:3000");
     //console.log("👂 Sirviendo en http://${process.env.IP}:${process.env.PORT}");
 })
