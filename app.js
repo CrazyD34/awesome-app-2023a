ddebb8d4eae5c26da6b06233579e7a03cbcd1093
CrazyD34 <pericopter67@gmail.com>
2023-05-12 16:05:33 +0000

    👾Middleware especifico y genericos

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ import express from 'express';
 //Crear una instancia de express
 const app = express(); //Express es un middleware, incluyen (request,response)
 
-//Registrando el primer middleware con una funcion
-
+//Registrando el primer middleware con una funcion estos son middleware genericos
 app.use((request,response,next) => {
     console.log("Este es el primer middleware");
     next();
@@ -16,6 +15,16 @@ app.use( (request,response,next) => {
     console.log(`${request.method} - ${request.url}`)
     next();
 })
+
+//MiddleWare de proposito espeficico iran al principio?
+app.use('/about', (request, response) => {
+    response.send(`
+    <h1 style="color:teal">About...</h1>
+    <p>Esta es una pagina creada para aprender 
+    desarrollo web en FullStack con JS</p>
+    <p>Otro parrafo</p>
+    `);
+});
 
 //Respondiendo al cliente
 app.use( (request,response) => {
